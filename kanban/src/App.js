027496ad2e62e88c250b07b0ac980dbd7776d5c1
Sara Pettigrew <sarapettigrew@Saras-MacBook-Pro.local>
2020-05-08 12:35:18 -0600

    Kanban board completed

--- kanban/src/App.js
+++ kanban/src/App.js
@@ -10,10 +10,56 @@ const itemsFromBe = [
 const columnsFromBe =
   {
     [uuid()]: {
-      name: 'Todo',
+      name: "Requested",
       items: itemsFromBe
+  },
+    [uuid()]: {
+      name: "In Progress",
+      items: []
+    }, 
+  [uuid()]: {
+    name: 'To do',
+    items: []
+  },
+  [uuid()]: {
+    name: 'Done',
+    items: []
+  },
+}
+
+const onDragEnd = (result, columns, setColumns) => {
+  if (!result.destination) return;
+  const { source, destination } = result;
+  if(source.droppableId !== destination.droppableId) {
+    const sourceColumn = columns[source.droppableId]
+    const destColumn = columns[destination.droppableId]
+    const sourceItems = [...sourceColumn.items]
+    const destItems = [...destColumn.items]
+    const [removed] = sourceItems.splice(source.index, 1)
+    destItems.splice(destination.index, 0, removed)
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        items: sourceItems
+      },
+      [destination.droppableId] : {
+        ...destItems,
+        items:destItems
+      }
+    })
+  } else {
+  const column = columns[source.droppableId]
+  const copiedItems = [...column.items]
+  const [removed] = copiedItems.splice(source.index, 1)
+  copiedItems.splice(destination.index, 0, removed)
+  setColumns({
+    ...columns,
+    [source.droppableId] : {
+    items: copiedItems
     }
-  }
+  })
+}
+}
 
 function App() {
   const [columns, setColumns] = useState(columnsFromBe);
@@ -26,6 +72,13 @@ function App() {
       <DragDropContext onDragEnd={result => console.log(result)}>
         {Object.entries(columns.map(([id, column]) => {
           return (
+            <div style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}>
+              <h2>{column.name}</h2>
+              <div style={{ margin: 8 }}>
             <Droppable droppableId={id} key={id}>
               {(provided, snapshot) => {
                 return (
@@ -70,6 +123,8 @@ function App() {
               }}
 
             </Droppable>
+            </div>
+          </div>
           )
         }))}
       </DragDropContext>
